8fee4db356c444af77baf3eeda6c41a296544c4e
Matteo Caruso <matteo.caruso7@studio.unibo.it>
2025-10-28 13:25:51 +0100

    Implemented different callback methods

--- Header Files/InputEvents.h
+++ Header Files/InputEvents.h
@@ -3,19 +3,22 @@
 #include <vector>
 #include <GLFW/glfw3.h>
 
+#include "InputEventsType.h"
+
 using std::vector;
 
 class InputEvents
 {
 public:
 	static void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
-	static void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods);
+	static void cursorPositionCallback(GLFWwindow *window, double xpos, double ypos);
+	static void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 	// Add more callbacks as needed here
 
-	vector<bool> *getInputEvents() { return &inputEvents; }
+	vector<bool> *getInputEvents() { return inputEvents; }
 
 private:
-	static vector<bool> inputEvents;
+	static vector<bool> *inputEvents;
 
 	InputEvents() = default;
 	~InputEvents() = default;
--- Source Files/InputEvents.cpp
+++ Source Files/InputEvents.cpp
@@ -1,1 +1,15 @@
 #include "../Header Files/InputEvents.h"
+
+vector<bool> *InputEvents::inputEvents = new vector<bool>(static_cast<size_t>(InputEventsType::count), false);
+
+void InputEvents::keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+}
+
+void InputEvents::cursorPositionCallback(GLFWwindow *window, double xpos, double ypos)
+{
+}
+
+void InputEvents::framebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+}
